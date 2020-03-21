d74661f4f41f0ec8eacfb042e8a0378b4a91c0f3
Juan Pablo Cánepa <juanpablo.canepa21@gmail.com>
2020-03-21 13:13:50 -0500

    Modify client

--- src/main/java/com/dev/pd/structural/facade/Cliente.java
+++ src/main/java/com/dev/pd/structural/facade/Cliente.java
@@ -1,12 +1,14 @@
 package com.dev.pd.structural.facade;
 
 public class Cliente {
-	
+
 	public static void main(String[] args) {
 		PedidoMenuFacade pedidoMenuFacade = new PedidoMenuFacade();
 		pedidoMenuFacade.servirOnlySopa();
 		pedidoMenuFacade.servirOnlyFondo();
-		pedidoMenuFacade.servirRefrescoYPostre();
+
+		PedidoMenuFacade serverRefresco = new PedidoMenuFacade();
+		serverRefresco.servirRefrescoYPostre();
 	}
 
 }
